616db92b302d847444429ea0e14256dd438f3ca1
Yesid Castro Calle <castroyesid17@gmail.com>
2021-02-14 02:47:46 -0800

    💀
    last commit for this branch

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
-import time
-from flask import Flask
+import io
 
-app = Flask(__name__)
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+print("Hello world")
